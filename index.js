ecfcea8e9c492c15117fac3aa5db121b4a41f28b
wujju <haowu90@gmail.com>
2017-08-28 00:33:41 +0800

    express version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/',(req,res)=>{
 	// res.sendFile(path.join(__dirname+'/Love.html'));
 	res.send('hello wolrd')
 })
-var server=app.listen(8001,()=>{
+var server=app.listen(8001,process.argv[2],()=>{
 	var host=server.address().address;
 	var port=server.address().port;
 	console.log("Running on at http://%s:%s",host,port);
