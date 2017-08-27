c8b299b9189bb92ff5535901ab814d3451a80b94
wujju <haowu90@gmail.com>
2017-08-28 00:55:17 +0800

    express version3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname+'/Love.html'));
 	// res.send('hello wolrd')
 })
+app.use(express.static('Love_files'));
 var server=app.listen(3000,process.argv[2],()=>{
 	var host=server.address().address;
 	var port=server.address().port;
